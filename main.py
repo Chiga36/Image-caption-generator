96f522f7fd882c4c46352c19de57bdf6bdccf366
Chiga36 <chigaswami@gmail.com>
2025-09-01 15:22:17 +0530

    done

---
+++ main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import matplotlib.pyplot as plt
+import pickle
+
+# caption generation function
+import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import matplotlib.pyplot as plt
+import pickle
+
+# load save files
+model_path = "model.keras"
+tokenizer_path = "tokenizer.pkl"
+feature_extractor_path = "feature_extractor.keras"
+
+
+def generate_and_display_caption(image_path, model_path, tokenizer_path, feature_extractor_path, max_length=23, img_size=224):
+    # Load the trained models and tokenizer
+    caption_model = load_model(model_path)
+    feature_extractor = load_model(feature_extractor_path)
+
+    with open(tokenizer_path, "rb") as f:
+        tokenizer = pickle.load(f)
+
+    # Preprocess the image
+    img = load_img(image_path, target_size=(img_size, img_size))
+    img = img_to_array(img) / 255.0  # Normalize pixel values
+    img = np.expand_dims(img, axis=0)
+    image_features = feature_extractor.predict(img, verbose=0)  # Extract image features
+    
+    # Generate the caption
+    in_text = "startseq"
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = caption_model.predict([image_features, sequence], verbose=0)
+        yhat_index = np.argmax(yhat)
+        word = tokenizer.index_word.get(yhat_index, None)
+        if word is None:
+            break
+        in_text += " " + word
+        if word == "endseq":
+            break
+    caption = in_text.replace("startseq", "").replace("endseq", "").strip()
+
+    # Display the image with the generated caption
+    img = load_img(image_path, target_size=(img_size, img_size))
+    plt.figure(figsize=(8, 8))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.title(caption, fontsize=16, color='blue')
+    # plt.show() this was for local testing .ipynb or kaggle notebook
+    st.pyplot(plt) # for grnrrating matplot image in streamlit app
+
+# custom main function
+def main():
+    st.title("Image captions generator")
+    st.write("Upload an image and get a caption generated for it.")
+    
+    uplaoded_image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+    
+    if uplaoded_image is not None:
+        # save image temporarily
+        with open("uploaded_image.jpg","wb") as f:
+            f.write(uplaoded_image.getbuffer())
+        # load image
+        model_path = "models/model.keras"    
+        feature_path = "models/feature_extractor.keras"    
+        tokenizer_path = "models/tokenizer.pkl"    
+        
+        # generate caption and display image
+        generate_and_display_caption('uploaded_image.jpg', model_path, feature_path, tokenizer_path)
+    
+if __name__ == "__main__":
+    main()
